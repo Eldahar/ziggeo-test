31e8804c75eb6423fc95d08c4d14b17eda26bb00
Tibor Oravecz <tibor.oravecz@gwshub.com>
2018-01-17 13:47:46 +0100

    to HTML

---
+++ src/test_player.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once "../vendor/autoload.php";
+
+$token = "ae8d7a5b95c8a8b5d5958f599dea4d14";
+
+$ziggeo = new Ziggeo(
+    'dc39fca5434c0532bee964012181ca04',
+    '944e4e74eceeb43cd46ef15ace3f5134',
+    '802376861b2dced1401381bf77864346'
+);
+$token = $ziggeo->auth()->generate(
+    [
+        "grants" => [
+            "read" => [
+                "session_owned" => true
+            ]
+        ],
+        "expiration_date" => date("U")+100,
+        "volatile" => true
+    ]
+);
+
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="https://assets-cdn.ziggeo.com/v2-stable/ziggeo.css" />
+    <script src="https://assets-cdn.ziggeo.com/v2-stable/ziggeo.js"></script>
+    <script>var app = new ZiggeoApi.V2.Application({
+            token: "dc39fca5434c0532bee964012181ca04",
+            webrtc_streaming: true,
+            auth: true,
+            "client-auth": "<?php echo $token ?>"
+        });
+    </script>
+</head>
+<body>
+
+<div id="replace_me-v2_player"></div>
+<script>
+    ZiggeoApi.Events.on("system_ready", function() {
+        var player = new ZiggeoApi.V2.Player({
+            element: document.getElementById("replace_me-v2_player"),
+            attrs: {
+                width: 320,
+                height: 180,
+                theme: "modern",
+                themecolor: "red",
+                video: "_sample_video"
+            }
+        });
+
+        player.activate();
+    });
+</script>
+</body>
+</html>

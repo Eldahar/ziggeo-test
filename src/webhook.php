fe864f86287360d3476eb3126d450d62f37c2d18
Tibor Oravecz <tibor.oravecz@gwshub.com>
2018-01-17 13:53:43 +0100

    to HTML

---
+++ src/webhook.php
@@ -0,0 +1,13 @@
+<?php
+
+$filename = sprintf("/tmp/%s", date("U"));
+
+file_put_contents(
+    $filename."_request.log",
+    var_export($_REQUEST, true)
+);
+
+file_put_contents(
+    $filename."_server.log",
+    var_export($_SERVER, true)
+);
